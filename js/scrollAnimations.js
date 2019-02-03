8216a088fd26d36cbcc3464b414637bd3fecb7ce
RagOfJoes <vhsvragojos@gmail.com>
2019-02-03 02:43:24 -0800

    Changed some class names.

--- js/scrollAnimations.js
+++ js/scrollAnimations.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
     var controller = new ScrollMagic.Controller();
     var tween = new TimelineLite();
-    tween.to('.overlayAboutInnerOne', .35, { height: '0vh', ease: Sine.easeOut }, 'first')
-        .to('.overlayTwo', .35, { height: '0vh', ease: Sine.easeOut }, 'first')
-        .from('.overlayAboutInner', .35, { width: '45.6vw', ease: Sine.easeOut }, 'second')
-        .from('.overlay', .35, { width: '30vw', ease: Sine.easeOut }, 'second');
+    tween.to('.aboutDarkOverlay', .35, { height: '0vh', ease: Sine.easeOut }, 'first')
+        .to('.personDarkOverlay', .35, { height: '0vh', ease: Sine.easeOut }, 'first')
+        .from('.aboutLightOverlay', .35, { width: '45.6vw', ease: Sine.easeOut }, 'second')
+        .from('.personLightOverlay', .35, { width: '30vw', ease: Sine.easeOut }, 'second');
 
     var scene = new ScrollMagic.Scene({
         triggerElement: '#about #person',
