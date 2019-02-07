9074d4ef9f037c8b17e30a0213de168a31025e6c
RagOfJoes <vhsvragojos@gmail.com>
2019-02-07 09:05:48 -0800

    added section slides animation to projects.

--- js/scrollAnimations.js
+++ js/scrollAnimations.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
     var controller = new ScrollMagic.Controller();
-    var tween = new TimelineLite();
-    tween.to('.aboutDarkOverlay', .35, { height: '0vh', ease: Sine.easeOut }, 'first')
-        .to('.personDarkOverlay', .35, { height: '0vh', ease: Sine.easeOut }, 'first')
-        .to('.aboutLightOverlay', .35, { width: '0px', ease: Sine.easeOut }, 'second')
-        .from('.personLightOverlay', .35, { width: '450px', ease: Sine.easeOut }, 'second');
+    var aboutTween = new TimelineLite();
+    aboutTween.to('#aboutTextContainer', .35, { height: '400px', ease: Sine.easeOut }, 'first')
+        .from('.aboutLightOverlay', .35, { height: '0px', ease: Sine.easeOut }, 'first')
+        .from('#personPicture', .35, { height: '0px', ease: Sine.easeOut }, 'first')
+        .from('.personLightOverlay', .35, { height: '0px', ease: Sine.easeOut }, 'first')
+        .to('.personLightOverlay', .35, { width: '0px', ease: Sine.easeOut }, 'second')
+        .to('.aboutLightOverlay', .35, { width: '0px', ease: Sine.easeOut }, 'second');
 
-    var scene = new ScrollMagic.Scene({
+    new ScrollMagic.Scene({
         triggerElement: '#about #personContainer',
-        triggerHook: 0.6,
+        triggerHook: 0.4,
     })
         .addIndicators({
             name: 'test',
@@ -16,6 +18,26 @@ $(document).ready(function () {
             colorStart: 'red',
             colorTrigger: 'black'
         })
-        .setTween(tween)
+        .setTween(aboutTween)
+        .addTo(controller);
+
+    var projectsTween = new TimelineLite();
+    projectsTween.to('#projectsContainer', .5, { z: -150 })
+        .to('#projectsContainer', 1, { x: '-50%' })
+        .to('#projectsContainer', .5, { z: 0 });
+
+    new ScrollMagic.Scene({
+        triggerElement: '#projects',
+        triggerHook: 'onLeave',
+        duration: '500%'
+    })
+        .addIndicators({
+            name: 'test2',
+            indent: 200,
+            colorStart: 'blue',
+            colorTrigger: 'pink'
+        })
+        .setPin('#projects')
+        .setTween(projectsTween)
         .addTo(controller);
 });
