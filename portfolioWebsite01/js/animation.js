7023dfd9feaf78178b5d1b0c0a5124257c003014
Victor Ragojos <ragofjoes@victors-macbook.attlocal.net>
2019-02-15 04:02:35 -0800

    added animations to contact page. tweaked project animations.

--- portfolioWebsite01/js/animation.js
+++ portfolioWebsite01/js/animation.js
@@ -12,7 +12,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
 var pathFinderText = new TimelineLite();
 $('.pathFinderTextContainer').mouseover(function () {
-    pathFinderText.to('.pathFinderText', .8, { autoAlpha: '0', y: '-600px' });
+    pathFinderText.to('.pathFinderText', .4, { autoAlpha: '0', y: '-500px' });
 }).mouseleave(function () {
-    pathFinderText.to('.pathFinderText', .8, { autoAlpha: '1', y: '0px', ease: Elastic.easeOut.config(.4, .2) });
-})
+    pathFinderText.to('.pathFinderText', .4, { autoAlpha: '1', y: '0px', ease: Elastic.easeOut.config(.4, .5) });
+});
+
+$('.buttonContainer').each(function (index, element) {
+    var contactAnim = new TimelineLite({ paused: true });
+    contactAnim.to($(element), .2, { width: '60%' });
+    contactAnim.to($(element).find('.footerButton'), .5, { letterSpacing: '.4em' }, 'together');
+    contactAnim.to($(element).find('.footerButton'), .2, { autoAlpha: '0' }, 'together');
+
+    element.animation = contactAnim;
+});
+
+$('.buttonContainer').mouseenter(function () {
+    this.animation.play();
+}).mouseleave(function () {
+    this.animation.reverse();
+});
